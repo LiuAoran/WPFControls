726b5d6a807441e8f7fd0beebd925e3b4a7874fc
LiuAoran <aoran_liu@outlook.com>
2023-04-24 00:59:42 +0800

    TrayApp

---
+++ WPFControls/TrayApp/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TrayApp
+{
+    internal class MainWindowViewModel: ObservableRecipient
+    {
+        private bool _showInTaskbar;
+        public bool ShowInTaskbar
+        {
+            get => _showInTaskbar;
+            set => SetProperty(ref _showInTaskbar, value);
+        }
+
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get => _windowState;
+            set
+            {
+                ShowInTaskbar = true;
+                SetProperty(ref _windowState, value);
+                ShowInTaskbar = value != WindowState.Minimized;
+            }
+        }
+
+        private NotifyIconWrapper.NotifyRequestRecord? _notifyRequest;
+        public NotifyIconWrapper.NotifyRequestRecord? NotifyRequest
+        {
+            get => _notifyRequest;
+            set => SetProperty(ref _notifyRequest, value);
+        }
+
+        public ICommand LoadedCommand { get; }
+        public ICommand ClosingCommand { get; }
+        public ICommand NotifyCommand { get; }
+        public ICommand NotifyIconOpenCommand { get; }
+        public ICommand NotifyIconExitCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            LoadedCommand = new RelayCommand(Loaded);
+            ClosingCommand = new RelayCommand<CancelEventArgs>(Closing);
+            NotifyCommand = new RelayCommand(()=>Notify("Hello World"));
+            NotifyIconOpenCommand = new RelayCommand(()=>WindowState = WindowState.Normal);
+            NotifyIconExitCommand = new RelayCommand(() => App.Current.Shutdown());
+        }
+
+        private void Notify(string message)
+        {
+            NotifyRequest = new NotifyIconWrapper.NotifyRequestRecord
+            {
+                Title = "Notify",
+                Text = message,
+                Duration = 1000
+            };
+        }
+
+        private void Loaded()
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Closing(CancelEventArgs? e)
+        {
+            if (e == null)
+                return;
+            e.Cancel = true;
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
