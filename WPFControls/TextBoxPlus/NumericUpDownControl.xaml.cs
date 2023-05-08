4c1b06eb937a4d572b5494f204e9aae0bdd58da0
LiuAoran <aoran_liu@outlook.com>
2023-05-08 09:30:42 +0800

    NumericUpDown

--- WPFControls/TextBoxPlus/NumericUpDownControl.xaml.cs
+++ WPFControls/TextBoxPlus/NumericUpDownControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,82 @@ namespace TextBoxPlus
     /// </summary>
     public partial class NumericUpDownControl : UserControl
     {
+        private string regixString = "[^0-9]+";
+
+        // The dependency property which will be accessible on the UserControl
+        public static readonly DependencyProperty UnitProperty =
+            DependencyProperty.Register("Unit", typeof(string), typeof(NumericUpDownControl), new UIPropertyMetadata());
+
+        public string Unit
+        {
+            get { return (string)GetValue(UnitProperty); }
+            set { SetValue(UnitProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaxiumProperty =
+           DependencyProperty.Register("Maxium", typeof(int), typeof(NumericUpDownControl), new UIPropertyMetadata());
+        public int Maxium
+        {
+            get { return (int)GetValue(MaxiumProperty); }
+            set { SetValue(MaxiumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimumProperty =
+    DependencyProperty.Register("Minimum", typeof(int), typeof(NumericUpDownControl), new UIPropertyMetadata());
+        public int Minimum
+        {
+            get { return (int)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
         public NumericUpDownControl()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex(regixString).IsMatch(e.Text);
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyStates == Keyboard.GetKeyStates(Key.LeftCtrl) || e.KeyStates == Keyboard.GetKeyStates(Key.RightCtrl)) && e.KeyStates == Keyboard.GetKeyStates(Key.V))
+                e.Handled = true;
+            else
+                e.Handled = false;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBox.Text))
+            {
+                if (int.Parse(TextBox.Text) > Maxium)
+                {
+                    TextBox.Text = Maxium.ToString();
+                }
+
+                if (int.Parse(TextBox.Text) < Minimum)
+                {
+                    TextBox.Text = Minimum.ToString();
+                }
+            }
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox.Text, out int num))
+            {
+                TextBox.Text = (++num).ToString();
+            }
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TextBox.Text, out int num))
+            {
+                TextBox.Text = (--num).ToString();
+            }
         }
     }
 }
