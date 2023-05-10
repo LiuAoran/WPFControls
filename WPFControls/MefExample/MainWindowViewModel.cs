adcd7d9ca69e6f2a80b81f06ce7ff456c0c77f72
LiuAoran <aoran_liu@outlook.com>
2023-05-11 00:56:38 +0800

    mef start

---
+++ WPFControls/MefExample/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Composition;
+using System.Composition.Hosting;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Controls;
+
+namespace MefExample
+{
+    public class MainWindowViewModel
+    {
+        private MenuItemObject _selectedPlugin;
+
+        public MenuItemObject SelectedPlugin
+        {
+            get => _selectedPlugin; 
+            private set => _selectedPlugin = value;
+        }
+
+        public ICommand LoadPluginCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            LoadPluginCommand = new RelayCommand(LoadPlugin);
+        }
+
+        private void LoadPlugin()
+        {
+
+        }
+    }
+}
---
+++ WPFControls/MefExample/MenuItemObject.cs
@@ -0,0 +1,23 @@
+﻿using MefExample.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MefExample
+{
+    public class MenuItemObject
+    {
+        public string Name { get; set; }
+        public Lazy<IPlugin> Plugin { get; set; }
+
+        public ICommand Command { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
