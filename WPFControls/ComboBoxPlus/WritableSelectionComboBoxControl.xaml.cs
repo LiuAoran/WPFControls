a74603cf45086bd67bea4ba32154d6def69ca031
LiuAoran <aoran_liu@outlook.com>
2023-06-05 20:01:30 +0800

    ComboBox

--- WPFControls/ComboBoxPlus/WritableSelectionComboBoxControl.xaml.cs
+++ WPFControls/ComboBoxPlus/WritableSelectionComboBoxControl.xaml.cs
@@ -16,10 +16,29 @@ using System.Windows.Shapes;
 namespace ComboBoxPlus
 {
     /// <summary>
-    /// WritableSelectionComBoxControl.xaml 的交互逻辑
+    /// WritableSelectionComboBoxControl.xaml 的交互逻辑
     /// </summary>
     public partial class WritableSelectionComboBoxControl : UserControl
     {
+        public string SelectedIndex
+        {
+            get { return (string)GetValue(SelectedIndexProperty); }
+            set {
+                SetValue(SelectedIndexProperty, value); 
+            }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(string), typeof(WritableSelectionComboBoxControl), new PropertyMetadata("0"));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(WritableSelectionComboBoxControl), new PropertyMetadata(""));
 
 
         public WritableSelectionComboBoxControl()
@@ -27,29 +46,17 @@ namespace ComboBoxPlus
             InitializeComponent();
         }
 
-        private void WritableSelectionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-        }
-
-        private void WritableSelectionTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void WritableSelectionTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void WritableSelectionTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void WritableSelectionTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-
+            var comboBox = sender as ComboBox;
+            if(comboBox.SelectedIndex == comboBox.Items.Count - 1)
+            {
+                TextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                TextBox.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
