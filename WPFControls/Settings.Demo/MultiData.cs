337ffb928ac34919b479636f6650840520c505ba
LiuAoran <aoran_liu@outlook.com>
2023-06-25 19:58:17 +0800

    Begining of list^&Tree, settings

---
+++ WPFControls/Settings.Demo/MultiData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Settings.Demo
+{
+    public class MultiData
+    {
+        public string Msg = string.Empty;
+        public Color Color;
+    }
+
+    public class MultiDataList: List<MultiData>
+    {
+
+    }
+}
