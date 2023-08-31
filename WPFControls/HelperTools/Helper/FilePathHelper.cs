7ef638b4e6fa562c3b650541917ce490558fb4c8
liuaoran <liuaoran@kdanmobile.com>
2023-08-31 18:07:42 +0800

    up

---
+++ WPFControls/HelperTools/Helper/FilePathHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperTools.Helper
+{
+    public static class FilePathHelper
+    {
+        /// <summary>
+        /// Returns the file size based on the specified file path, with the smallest unit being bytes (B).
+        /// </summary>
+        /// <param name="filePath">The path to the file.</param>
+        /// <returns>
+        /// The calculated file size, with units in bytes (B), kilobytes (KB), megabytes (MB), or gigabytes (GB).
+        /// </returns>
+        public static string GetFileSize(string filePath)
+        {
+            try
+            {
+                long fileSize = new FileInfo(filePath).Length;
+                string[] sizes = { "B", "KB", "MB", "GB" };
+                int order = 0;
+
+                while (fileSize >= 1024 && order < sizes.Length - 1)
+                {
+                    fileSize /= 1024;
+                    order++;
+                }
+
+                return $"{fileSize} {sizes[order]}";
+            }
+            catch
+            {
+                return "0B";
+            }
+        }
+
+
+        public enum FileDialogType
+        {
+            Open,
+            Save
+        }
+
+        public static string GetFilePathOrEmpty(FileDialogType dialogType, string filter)
+        {
+            string selectedFilePath = string.Empty;
+            FileDialog fileDialog = dialogType switch
+            {
+                FileDialogType.Open => new OpenFileDialog(),
+                FileDialogType.Save => new SaveFileDialog(),
+                _ => throw new ArgumentOutOfRangeException(nameof(dialogType), "Invalid dialog type."),
+            };
+            fileDialog.Filter = filter;
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                selectedFilePath = fileDialog.FileName;
+            }
+
+            return selectedFilePath;
+        }
+    }
+}
