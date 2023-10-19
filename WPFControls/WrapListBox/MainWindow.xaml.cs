15b2e9b83c0e736b44c608d61e64b8ccb63f9846
LiuAoran <aoran_liu@outlook.com>
2023-10-20 02:32:28 +0800

    WrapListBox

---
+++ WPFControls/WrapListBox/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WrapListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateItem();
+        }
+         
+
+        private void CreateItem()
+        {
+            if (WrapListBoxControl != null)
+            {
+                WrapListBoxControl.Items.Add(new CustomItem
+                {
+                    TitleText = "A1",
+                    DescriptionText = "A1 is a1"
+                });
+
+                WrapListBoxControl.Items.Add(new CustomItem
+                {
+                    TitleText = "A2",
+                    DescriptionText = "A2 is a2"
+                });
+
+                WrapListBoxControl.Items.Add(new CustomItem
+                {
+                    TitleText = "A3",
+                    DescriptionText = "A2 is a2"
+                });
+
+                WrapListBoxControl.Items.Add(new CustomItem
+                {
+                    TitleText = "A4",
+                    DescriptionText = "A4 is a4"
+                });
+
+                WrapListBoxControl.Items.Add(new CustomItem
+                {
+                    TitleText = "A5",
+                    DescriptionText = "A5 is a5"
+                });
+                WrapListBoxControl.SelectionChanged += WrapListBoxControl_SelectionChanged; ;
+            }
+        }
+
+        private void WrapListBoxControl_SelectionChanged(object? sender, CustomItem e)
+        {
+            MessageBox.Show(e.TitleText);
+        }
+    }
+}
---
+++ WPFControls/WrapListBox/WrapListBoxControl.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WrapListBox
+{
+    /// <summary>
+    /// WrapListBoxControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class WrapListBoxControl : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public event EventHandler<CustomItem> SelectionChanged;
+
+        public static readonly DependencyProperty ItemsProperty =
+           DependencyProperty.Register("Items", typeof(ObservableCollection<CustomItem>), typeof(WrapListBoxControl), new PropertyMetadata(null));
+
+        public ObservableCollection<CustomItem> Items
+        {
+            get { return (ObservableCollection<CustomItem>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public WrapListBoxControl()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+            Items = new ObservableCollection<CustomItem>();
+            WrapListBox.SelectionChanged += WrapListBox_SelectionChanged;
+        }
+
+        private void WrapListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            if (listBox != null && listBox.SelectedIndex != -1) {
+                SelectionChanged?.Invoke(sender, Items[listBox.SelectedIndex]);
+                listBox.SelectedIndex= -1;
+            } 
+        }
+
+        protected void UpdateProper<T>(ref T properValue,
+                    T newValue,
+                    [CallerMemberName] string properName = "")
+        {
+            if (object.Equals(properValue, newValue))
+                return;
+
+            properValue = newValue;
+            OnPropertyChanged(properName);
+
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CustomItem : Control, INotifyPropertyChanged
+    {
+
+        private string _titleText;
+        public string TitleText
+        {
+            get => _titleText;
+            set
+            {
+                UpdateProper(ref _titleText, value);
+            }
+        }
+
+        private string _descriptionText;
+        public string DescriptionText
+        {
+            get => _descriptionText;
+            set
+            {
+                UpdateProper(ref _descriptionText, value);
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void UpdateProper<T>(ref T properValue,
+                            T newValue,
+                            [CallerMemberName] string properName = "")
+        {
+            if (object.Equals(properValue, newValue))
+                return;
+
+            properValue = newValue;
+            OnPropertyChanged(properName);
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
