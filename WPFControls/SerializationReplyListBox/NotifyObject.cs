b4e9d4141bd1cf3e9869e6514a6b303cccd6ce55
youna <aoran_liu@outlook.com>
2024-07-24 18:13:51 +0800

    SerializationReplyListBox - primary

---
+++ WPFControls/SerializationReplyListBox/NotifyObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationReplyListBox
+{
+    public class NotifyObject: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void UpdateProper<T>(ref T properValue,
+                            T newValue,
+                            [CallerMemberName] string properName = "")
+        {
+
+            properValue = newValue;
+            OnPropertyChanged(properName);
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+}
